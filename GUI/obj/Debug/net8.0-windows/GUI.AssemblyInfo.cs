b7427d72d292e9d76a45a3399866a6c9a1526628
Sasha Veiber <alexandrveiber@gmail.com>
2026-03-07 15:21:00 +0700

    readme 2.0

--- GUI/obj/Debug/net8.0-windows/GUI.AssemblyInfo.cs
+++ GUI/obj/Debug/net8.0-windows/GUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aac1bf588a0dc651fdb84e8523baacb4d8413447")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b96ed96868f0b05064417764904299f0cf6fc2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("GUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
